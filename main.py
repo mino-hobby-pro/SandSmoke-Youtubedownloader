0bcc66354c07d53ad7d8535f5cda33438242b537
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-11 16:16:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from pytube import YouTube
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/video', methods=['GET'])
-def video():
+def get_video_stream():
     video_id = request.args.get('id')
-    if video_id:
-        try:
-            url = f'https://www.youtube.com/watch?v={video_id}'
-            yt = YouTube(url)
-            stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
-            return jsonify({'stream_url': stream.url})
-        except Exception as e:
-            return jsonify({'error': str(e)}), 400
-    return jsonify({'error': 'No video ID provided.'}), 400
+    if not video_id:
+        return jsonify({'error': 'Video ID is required'}), 400
+    
+    try:
+        yt = YouTube(video_id)
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
+        stream_url = stream.url
+        return jsonify({'stream_url': stream_url})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
