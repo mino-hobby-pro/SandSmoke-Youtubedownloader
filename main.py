ccaf0f20cfaf7bd8a2f634128cfa912c1df432f7
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-17 16:51:44 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form['url']
+        try:
+            response = requests.get(url)
+            return render_template('index.html', content=response.text)
+        except requests.exceptions.RequestException as e:
+            return render_template('index.html', error=str(e))
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
